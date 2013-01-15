c7cccd03f813eb08d2efa4296fc666c78898a469
Novikov Andrey <envek@envek.name>
2013-01-15 22:25:38 +1000

    A word about encodings

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QtGui/QApplication>
+#include <QTextCodec>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QApplication a(argc, argv);
     QUrl url;
     if (argc > 1)
