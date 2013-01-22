78481f333d89ad5096db9473d321b57681151f50
Novikov Andrey <envek@envek.name>
2013-01-22 14:56:36 +1000

    Parse URLs from command line parameters correctly

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QUrl url;
     if (argc > 1)
-        url = QUrl(argv[1]);
+        url = QUrl::fromUserInput(argv[1]);
     else
         url = QUrl("http://anifest-amur.ru/");
     MainWindow w(url);
